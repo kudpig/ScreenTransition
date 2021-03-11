97b26061cf762c968bd4d7fb1f6dc8532150e67c
kudpig <shin.kudo.aa@gmail.com>
2021-03-12 06:18:34 +0900

    main.storyboardの削除(info.plistも)

--- ScreenTransition/AppDelegate.swift
+++ ScreenTransition/AppDelegate.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
+// @mainの記述により、main.swiftを省略しつつmain関数を自動実装できる。
+// main関数とはアプリ起動時に最初に実行する関数。
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    // アプリの起動時に呼ばれる。よくわからんが初期化処理などを書く？
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
