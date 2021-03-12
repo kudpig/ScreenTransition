07a18d4b54b5a936c1df99c0fa2115d0ae9a64cb
kudpig <shin.kudo.aa@gmail.com>
2021-03-13 00:17:58 +0900

    AppDelegateの記述(初期画面表示まで)

--- ScreenTransition/AppDelegate.swift
+++ ScreenTransition/AppDelegate.swift
@@ -12,11 +12,25 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // アプリケーションのwindowをインスタンス化
     var window: UIWindow?
     
     // アプリの起動時に呼ばれる。よくわからんが初期化処理などを書く？
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds) // windowのサイズ。複数窓開く訳ではないので全画面
+        self.window = window // AppDelegateクラスにて定義した変数windowに、関数内で作成したwindowを代入
+        
+        // 最初のstoryboardを取得
+        let firststoryboard = UIStoryboard(name: "First", bundle: nil)
+        // storyboardから最初のViewControllerを取得
+        // instantiateInitialViewControllerはSBクラスのメソッドで、「is initial view controller」が設定されている ViewController を取得できる
+        let firstVC = firststoryboard.instantiateInitialViewController() as! FirstViewController
+        // ナビゲーションコントローラーを定義。引数で最下層のページを指定
+        let nav = UINavigationController(rootViewController: firstVC)
+        window.rootViewController = nav
+        window.makeKeyAndVisible()
+        
         return true
     }
 
