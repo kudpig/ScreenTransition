ab6261174832396a2fdd7464010923626c469279
kudpig <shin.kudo.aa@gmail.com>
2021-03-14 00:40:11 +0900

    画面遷移コードの記述(FirstからSecond)

--- ScreenTransition/First/FirstViewController.swift
+++ ScreenTransition/First/FirstViewController.swift
@@ -14,4 +14,13 @@ class FirstViewController: UIViewController {
 
     }
 
+    
+    @IBAction func tapGoSecond(_ sender: UIButton) {
+        let secondStoryboard = UIStoryboard(name: "Second", bundle: nil)
+        let secondVC = secondStoryboard.instantiateInitialViewController() as! SecondViewController
+        
+        let nav = self.navigationController
+        nav?.pushViewController(secondVC, animated: true)
+    }
+    
 }
